f98f545d6ac5c3927b417d46c1b662c05d09c6ae
rickyepuche <aedigin@byupathway.edu>
2025-10-05 22:16:37 +0100

    i completed the project siteplan and the site meets the requirements

--- scripts/temples.js
+++ scripts/temples.js
@@ -7,10 +7,7 @@ toggleButton.addEventListener("click", () =>{
 });
 
 let now = new Date();
-
 let year = now.getFullYear();
-
 document.querySelector("#currentYear").textContent = year;
-
 let date = now.toLocaleString();
 document.querySelector("#lastModified").textContent = date;
